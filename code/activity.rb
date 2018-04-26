d426e269e24d6b0426ac4e7cad0b8ee6281eac47
Jeff Nyman <jeffnyman@gmail.com>
2018-04-26 13:46:24 -0500

    Tests with durations.

--- code/activity.rb
+++ code/activity.rb
@@ -10,7 +10,16 @@ class Activity
     @finished
   end
 
-  def set_finished
-    @finished = true
+  def set_finished(date = Time.current)
+    @finished = date
+  end
+
+  def part_of_pace?
+    return false unless @finished
+    @finished > 14.days.ago
+  end
+
+  def counts_towards_pace
+    part_of_pace? ? cost : 0
   end
 end
--- spec/activity_spec.rb
+++ spec/activity_spec.rb
@@ -11,4 +11,25 @@ RSpec.describe Activity do
       expect(activity).to be_finished
     end
   end
+
+  describe "pace" do
+    let(:activity) { Activity.new(cost: 5) }
+
+    it "does not count unfinished activities toward pace" do
+      expect(activity).not_to be_part_of_pace
+      expect(activity.counts_towards_pace).to eq(0)
+    end
+
+    it "counts recently finished activities toward pace" do
+      activity.set_finished(1.day.ago)
+      expect(activity).to be_part_of_pace
+      expect(activity.counts_towards_pace).to eq(5)
+    end
+
+    it "does not count non-recent finished activities toward pace" do
+      activity.set_finished(1.month.ago)
+      expect(activity).not_to be_part_of_pace
+      expect(activity.counts_towards_pace).to eq(0)
+    end
+  end
 end
